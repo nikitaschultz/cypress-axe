60a0b1a3bbc2c9548f3c4ae16ac766bf696b41f9
Nikita Schultz <nikita.schultz1996@gmail.com>
2021-02-10 21:05:24 +0000

    added custom logs

--- cypress/integration/examples/test.spec.js
+++ cypress/integration/examples/test.spec.js
@@ -1,6 +1,30 @@
 /// <reference types="cypress" />
 
+
+
 context('Actions', () => {
+
+  function terminalLog(violations) {
+    cy.task(
+      'log',
+      `${violations.length} accessibility violation${
+        violations.length === 1 ? '' : 's'
+      } ${violations.length === 1 ? 'was' : 'were'} detected`
+    )
+    // pluck specific keys to keep the table readable
+    const violationData = violations.map(
+      ({ id, impact, description, nodes }) => ({
+        id,
+        impact,
+        description,
+        nodes: nodes.length
+      })
+    )
+  
+    cy.task('table', violationData)
+  }
+
+  
   it('should be accessible', () => {
     cy.visit('https://www.audio-unity-group.com/')
 
